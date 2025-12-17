451038969f82dbffd1bc5e2f4fe92491be12452d
Richmond Reglo <richmondreglo25@gmail.com>
2025-12-17 11:44:02 +0800

    Updates

--- _next/static/Ym5aYV3zoCeYDPnKUlnbY/_buildManifest.js
+++ _next/static/Ym5aYV3zoCeYDPnKUlnbY/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/ygophmeta//_next/:path+",
-        "destination": "/ygophmeta/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
