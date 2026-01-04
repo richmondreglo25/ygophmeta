cc9b3edf9c2b39a816af27bb8c298ebf1a0baa1b
Richmond Reglo <richmondreglo25@gmail.com>
2026-01-05 00:33:07 +0800

    Updates

---
+++ _next/static/_SaLwKVUOedVHL9JltEjg/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ygophmeta//_next/:path+",
+        "destination": "/ygophmeta/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
